7250b329f79e6544a8c0c25bf0914a7e242a1747
Trần Lâm <52204256+RevolNoom@users.noreply.github.com>
2026-02-21 11:45:52 +0700

    Use integer args in Morton2 set/add/sub component tests

--- godot_morton_code/src/morton2.gd
+++ godot_morton_code/src/morton2.gd
@@ -57,6 +57,31 @@ static func sub(lhs: int, rhs: int):
 	return ((x_diff & MASK_X) | (y_diff & MASK_Y))
 
 
+## Return a copy of [param code] with x-component added by [param x].[br]
+static func add_x(code: int, x: int) -> int:
+	var encoded_x = Morton2._encodeMB64(x)
+	var x_sum = (code | MASK_Y) + encoded_x
+	return (x_sum & MASK_X) | (code & MASK_Y)
+
+## Return a copy of [param code] with y-component added by [param y].[br]
+static func add_y(code: int, y: int) -> int:
+	var encoded_y = Morton2._encodeMB64(y) << 1
+	var y_sum = (code | MASK_X) + encoded_y
+	return (y_sum & MASK_Y) | (code & MASK_X)
+
+## Return a copy of [param code] with x-component subtracted by [param x].[br]
+static func sub_x(code: int, x: int) -> int:
+	var encoded_x = Morton2._encodeMB64(x)
+	var x_diff = (code & MASK_X) - encoded_x
+	return (x_diff & MASK_X) | (code & MASK_Y)
+
+## Return a copy of [param code] with y-component subtracted by [param y].[br]
+static func sub_y(code: int, y: int) -> int:
+	var encoded_y = Morton2._encodeMB64(y) << 1
+	var y_diff = (code & MASK_Y) - encoded_y
+	return (y_diff & MASK_Y) | (code & MASK_X)
+
+
 
 ## Return a copy of [param code] with x-component added by 1.[br]
 static func inc_x(code: int) -> int:
